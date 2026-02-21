a45d04524b03f295531c16ec0fbf3a31b6bf08a2
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2026-02-21 16:17:27 +0200

    Add unit test for dogService handling API 500 error

--- server/tests/dogService.test.ts
+++ server/tests/dogService.test.ts
@@ -1,6 +1,5 @@
 import {describe, test, vi, expect} from 'vitest';
 import {getRandomDogImage} from '../services/dogService.ts';
-import { ok } from 'assert';
 
 describe('Dog service tests', () => {
     test('Image url is same than the one returned by the API', async () => {
@@ -25,4 +24,14 @@ describe('Dog service tests', () => {
         expect(response.status).toBe('success');
         
     })
+
+    test('API returns an error status', async () => {
+        // Mocking the global fetch function to return an error response
+        global.fetch = vi.fn().mockResolvedValue({
+            ok: false,
+            status: 500
+        });
+        
+        await expect(getRandomDogImage()).rejects.toThrow('Failed to fetch dog image: Dog API returned status 500');
+    });
 });
