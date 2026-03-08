593214f0bd49198a1995275968229fbb24a937f7
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2026-03-08 22:45:31 +0200

    negative test for invalid route

--- server/tests/APItest/apiTest.test.ts
+++ server/tests/APItest/apiTest.test.ts
@@ -18,4 +18,17 @@ describe('Dog API tests', () => {
         // verify imageUrl contains string
         expect(typeof response.body.data.imageUrl).toBe('string');
     });
+
+    test('should return 404 for invalid route', async () => {
+        const response = await request(app)
+            .get('/api/dogs/random-invalid-route')
+
+        // verify response status is 404
+        expect(response.status).toBe(404);
+        // returned response contain error message
+        expect(response.body).toBeDefined();
+        expect(response.body.error).toBeDefined();
+        //  verify that returned error message is correct
+        expect(response.body.error).toBe('Route not found');
+    });
 });
